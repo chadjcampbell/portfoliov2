0dfaf961a42690766b02efa426a9418b0ead9f8d
chadjcampbell <chadjcampbell@gmail.com>
2023-10-24 15:48:21 -0400

    change boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>Hello World</div>;
-};
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
 
 export default App;
