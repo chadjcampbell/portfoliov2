d975f27a9c2ec8e0ee277c2571ed46b3446b05f2
chadjcampbell <chadjcampbell@gmail.com>
2023-10-27 18:48:05 -0400

    style the hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,6 +3,18 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>CHAD J CAMPBELL</h2>
+          <h1>Full Stack Web Developer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="scroll wheel" />
+        </div>
+      </div>
+      <div className="slidingTextContainer">Code - Create - Innovate</div>
       <div className="imageContainer">
         <img src="/cyberChadHero-noBG.png" alt="Chads Hero Image" />
       </div>
