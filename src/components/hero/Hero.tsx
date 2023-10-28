c4150ec082917e061dc2792873b52fc461e2d557
chadjcampbell <chadjcampbell@gmail.com>
2023-10-28 10:25:55 -0400

    add animations to hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,20 +1,79 @@
+import { motion } from "framer-motion";
 import "./hero.scss";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 1.5,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      duration: 20,
+      repeat: Infinity,
+      repeatType: "mirror",
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>CHAD J CAMPBELL</h2>
-          <h1>Full Stack Web Developer</h1>
-          <div className="buttons">
-            <button>See the Latest Works</button>
-            <button>Contact Me</button>
-          </div>
-          <img src="/scroll.png" alt="scroll wheel" />
-        </div>
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>CHAD J CAMPBELL</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Full Stack Web Developer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt="scroll wheel"
+            variants={textVariants}
+            animate="scrollButton"
+          />
+        </motion.div>
       </div>
-      <div className="slidingTextContainer">Code - Create - Innovate</div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Code - Create - Innovate
+      </motion.div>
       <div className="imageContainer">
         <img src="/cyberChadHero-noBG.png" alt="Chads Hero Image" />
       </div>
