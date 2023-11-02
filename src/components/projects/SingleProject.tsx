713fb204aa2a13d744c4850fe97451d0b650eab8
chadjcampbell <chadjcampbell@gmail.com>
2023-11-02 15:02:32 -0400

    finish projects animations

--- src/components/projects/SingleProject.tsx
+++ src/components/projects/SingleProject.tsx
@@ -14,13 +14,13 @@ const SingleProject = ({ project }: SingleProjectProps) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section ref={ref}>
+    <section>
       <div className="container">
         <div className="wrapper">
-          <div className="imgContainer">
+          <div className="imgContainer" ref={ref}>
             <img src={project.img} alt={`${project.name} screenshot`} />
           </div>{" "}
-          <motion.div className="textContainer">
+          <motion.div className="textContainer" style={{ y }}>
             <h3>{project.name}</h3>
             <p>{project.description}</p>
             <button>See demo</button>
